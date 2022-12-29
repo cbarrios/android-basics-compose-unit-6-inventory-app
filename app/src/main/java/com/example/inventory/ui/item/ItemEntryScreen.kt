27d1f048c1b99449e372507e7d1277328cbda74d
cbarrios <lalobarrios16@yahoo.com>
2022-12-29 13:35:17 -0500

    master: (3) Starter app overview

---
+++ app/src/main/java/com/example/inventory/ui/item/ItemEntryScreen.kt
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.inventory.ui.item
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.inventory.InventoryTopAppBar
+import com.example.inventory.R
+import com.example.inventory.ui.AppViewModelProvider
+import com.example.inventory.ui.navigation.NavigationDestination
+import com.example.inventory.ui.theme.InventoryTheme
+import java.util.*
+
+object ItemEntryDestination : NavigationDestination {
+    override val route = "item_entry"
+    override val titleRes = R.string.item_entry_title
+}
+
+@Composable
+fun ItemEntryScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    canNavigateBack: Boolean = true,
+    viewModel: ItemEntryViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    Scaffold(
+        topBar = {
+            InventoryTopAppBar(
+                title = stringResource(ItemEntryDestination.titleRes),
+                canNavigateBack = canNavigateBack,
+                navigateUp = onNavigateUp
+            )
+        }
+    ) { innerPadding ->
+        ItemEntryBody(
+            itemUiState = viewModel.itemUiState,
+            onItemValueChange = viewModel::updateUiState,
+            onSaveClick = { },
+            modifier = modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun ItemEntryBody(
+    itemUiState: ItemUiState,
+    onItemValueChange: (ItemUiState) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(32.dp)
+    ) {
+        ItemInputForm(itemUiState = itemUiState, onValueChange = onItemValueChange)
+        Button(
+            onClick = onSaveClick,
+            enabled = itemUiState.actionEnabled,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.save_action))
+        }
+    }
+}
+
+@Composable
+fun ItemInputForm(
+    itemUiState: ItemUiState,
+    modifier: Modifier = Modifier,
+    onValueChange: (ItemUiState) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(modifier = modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        OutlinedTextField(
+            value = itemUiState.name,
+            onValueChange = { onValueChange(itemUiState.copy(name = it)) },
+            label = { Text(stringResource(R.string.item_name_req)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = itemUiState.price,
+            onValueChange = { onValueChange(itemUiState.copy(price = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+            label = { Text(stringResource(R.string.item_price_req)) },
+            leadingIcon = { Text(Currency.getInstance(Locale.getDefault()).symbol) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = itemUiState.quantity,
+            onValueChange = { onValueChange(itemUiState.copy(quantity = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(stringResource(R.string.quantity_req)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ItemEntryScreenPreview() {
+    InventoryTheme {
+        ItemEntryBody(
+            itemUiState = ItemUiState(
+                name = "Item name",
+                price = "10.00",
+                quantity = "5"
+            ),
+            onItemValueChange = {},
+            onSaveClick = {}
+        )
+    }
+}
