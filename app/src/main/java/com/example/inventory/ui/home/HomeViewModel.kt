3c7b535face5e4f284e34437075d0936964a46bb
cbarrios <lalobarrios16@yahoo.com>
2022-12-29 15:20:21 -0500

    master: [2] (3) Update UI state

--- app/src/main/java/com/example/inventory/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/home/HomeViewModel.kt
@@ -17,16 +17,30 @@
 package com.example.inventory.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.inventory.data.Item
+import com.example.inventory.data.ItemsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 /**
  * View Model to retrieve all items in the Room database.
  */
-class HomeViewModel : ViewModel() {
+class HomeViewModel(itemsRepository: ItemsRepository) : ViewModel() {
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    val homeUiState: StateFlow<HomeUiState> =
+        itemsRepository.getAllItemsStream().map { HomeUiState(it) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = HomeUiState()
+            )
 }
 
 /**
